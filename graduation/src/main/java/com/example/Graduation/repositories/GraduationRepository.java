6326c3f44b8b03997da3e01e9739a360edf9af54
TranCongMinh172 <trancongminh1722002@gmail.com>
2024-05-16 11:31:48 +0700

    config project

--- graduation/src/main/java/com/example/Graduation/repositories/GraduationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.Graduation.repositories;
-
-import com.example.Graduation.models.Graduation;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface GraduationRepository extends JpaRepository<Graduation,Long> {
-}
--- school/src/main/java/com/example/school/repositories/SchoolRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.school.repositories;
-
-import com.example.school.models.School;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchoolRepository extends JpaRepository<School,Integer> {
-
-}
