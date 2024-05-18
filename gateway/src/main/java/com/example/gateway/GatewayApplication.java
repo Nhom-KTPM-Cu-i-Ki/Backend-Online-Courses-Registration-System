73afcfada296f9fe66f77f595b64ecaead384af7
dungngoo <ngodung06vn@gmail.com>
2024-05-18 18:21:03 +0700

    Spring role based authorization

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -2,8 +2,10 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.example.gateway","com.example.gateway.filter"})
 public class GatewayApplication {
 
 	public static void main(String[] args) {
